1a301a4b859bd8636563f62370729508f0d047b4
danielcuque <3024465830102@ingenieria.usac.edu.gt>
2024-03-05 20:08:29 -0600

    feat: docker compose file

--- Proyectos/Proyecto1/frontend/src/App.tsx
+++ Proyectos/Proyecto1/frontend/src/App.tsx
@@ -1,5 +1,22 @@
+const data = { "totalRam": 4102369280, "memoriaEnUso": 2563993600, "porcentaje": 62, "libre": 1538375680 }
+
 export const App = () => {
   return (
-    <div>App</div>
+    <>
+      <div className="grid grid-cols-2 gap-4 justify-center items-center h-screen p-8">
+          <button className="card">
+            Monitoreo
+          </button>
+          <button className="card">
+            Acciones
+          </button>
+          <button className="card">
+            Árbol de procesos
+          </button>
+          <button className="card">
+            Histórico
+          </button>
+      </div>
+    </>
   )
 }
