816c524a2cd5944f679bb9738ac1d5941d444304
danielcuque <3024465830102@ingenieria.usac.edu.gt>
2024-03-10 23:05:01 -0600

    feat: script to compile modules

--- Proyectos/Proyecto1/procs/compile_modules.sh
+++ Proyectos/Proyecto1/procs/compile_modules.sh
@@ -1,38 +1,36 @@
 #!/bin/bash
 
-# Verificar si se proporcionó al menos un nombre de módulo como argumento
-if [ $# -lt 1 ]; then
-    echo "Uso: $0 <nombre_del_modulo1> [<nombre_del_modulo2> ...]"
-    exit 1
-fi
-
-# Nombres de los módulos proporcionados como argumentos
-module_names=("$@")
+# Definir nombres de los módulos y sus ubicaciones de Makefile
+module_info=(
+    ["ram_so1_1s2024"]="Proyectos/Proyecto1/procs/ram"
+    ["cpu_so1_1s2024"]="Proyectos/Proyecto1/procs/cpu"
+)
 
 # Ejecutar Makefile para compilar los módulos
-make all
+for module_name in "${!module_info[@]}"; do
+    make -C "${module_info[$module_name]}" all
+    if [ $? -ne 0 ]; then
+        echo "Error de compilación para el módulo $module_name en ${module_info[$module_name]}. Abortando carga de módulos y eliminación de archivos."
+        exit 1
+    fi
+done
 
-# Verificar si la compilación fue exitosa
-if [ $? -eq 0 ]; then
-    echo "Compilación exitosa. Cargando los módulos..."
+echo "Compilación exitosa. Cargando los módulos..."
 
-    # Cargar los módulos
-    for module_name in "${module_names[@]}"; do
-        sudo insmod "$module_name.ko"
-        
-        # Verificar si el módulo se cargó correctamente
-        if [ $? -ne 0 ]; then
-            echo "Error al cargar el módulo $module_name. Abortando eliminación de archivos."
-            exit 1
-        fi
-    done
+# Cargar los módulos
+for module_name in "${!module_info[@]}"; do
+    sudo insmod "${module_info[$module_name]}/${module_name}.ko"
+    if [ $? -ne 0 ]; then
+        echo "Error al cargar el módulo $module_name. Abortando eliminación de archivos."
+        exit 1
+    fi
+done
 
-    echo "Módulos cargados correctamente. Eliminando archivos..."
-    
-    # Eliminar archivos generados
-    make clean
-    
-    echo "Proceso completado."
-else
-    echo "Error de compilación. Abortando carga de módulos y eliminación de archivos."
-fi
+echo "Módulos cargados correctamente. Eliminando archivos..."
+
+# Eliminar archivos generados
+for module_name in "${!module_info[@]}"; do
+    make -C "${module_info[$module_name]}" clean
+done
+
+echo "Proceso completado."
