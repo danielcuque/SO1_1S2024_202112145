96462900945843436d558a167dc49cefd1a351ae
danielcuque <3024465830102@ingenieria.usac.edu.gt>
2024-03-10 23:11:57 -0600

    feat: script to compile modules

--- Proyectos/Proyecto1/procs/compile_modules.sh
+++ Proyectos/Proyecto1/procs/compile_modules.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# Directorio base donde se encuentran los directorios de los módulos
-base_dir="$(dirname "$0")"
+# Directorio donde se encuentra el script
+script_dir="$(dirname "$0")"
 
-# Definir nombres de los módulos y sus ubicaciones de Makefile (relativas al directorio base)
+# Definir nombres de los módulos y sus ubicaciones de Makefile (relativas al directorio del script)
 module_info=(
-    ["ram_so1_1s2024"]="Proyectos/Proyecto1/procs/ram"
-    ["cpu_so1_1s2024"]="Proyectos/Proyecto1/procs/cpu"
+    ["ram_so1_1s2024"]="ram"
+    ["cpu_so1_1s2024"]="cpu"
 )
 
 # Ejecutar Makefile para compilar los módulos
 for module_name in "${!module_info[@]}"; do
-    make -C "$base_dir/${module_info[$module_name]}" all
+    make -C "$script_dir/${module_info[$module_name]}" all
     if [ $? -ne 0 ]; then
         echo "Error de compilación para el módulo $module_name en ${module_info[$module_name]}. Abortando carga de módulos y eliminación de archivos."
         exit 1
@@ -22,7 +22,7 @@ echo "Compilación exitosa. Cargando los módulos..."
 
 # Cargar los módulos
 for module_name in "${!module_info[@]}"; do
-    sudo insmod "$base_dir/${module_info[$module_name]}/${module_name}.ko"
+    sudo insmod "$script_dir/${module_info[$module_name]}/${module_name}.ko"
     if [ $? -ne 0 ]; then
         echo "Error al cargar el módulo $module_name. Abortando eliminación de archivos."
         exit 1
@@ -33,7 +33,7 @@ echo "Módulos cargados correctamente. Eliminando archivos..."
 
 # Eliminar archivos generados
 for module_name in "${!module_info[@]}"; do
-    make -C "$base_dir/${module_info[$module_name]}" clean
+    make -C "$script_dir/${module_info[$module_name]}" clean
 done
 
 echo "Proceso completado."
