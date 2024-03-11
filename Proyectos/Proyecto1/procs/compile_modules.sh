894601d943d8452f84681cf393720c9196e0416b
danielcuque <3024465830102@ingenieria.usac.edu.gt>
2024-03-10 23:19:16 -0600

    feat: script to compile modules

--- Proyectos/Proyecto1/procs/compile_modules.sh
+++ Proyectos/Proyecto1/procs/compile_modules.sh
@@ -1,39 +1,14 @@
 #!/bin/bash
+# Script para compilar los modulos y limpiarlos despues de insertarlos en /ram y /cpu
 
-# Directorio donde se encuentra el script
-script_dir="$(dirname "$0")"
+cd ram/
+make all
+sudo insmod ram_module.ko
+make clean
 
-# Definir nombres de los módulos y sus ubicaciones de Makefile (relativas al directorio del script)
-module_info=(
-    ["ram_so1_1s2024"]="ram"
-    ["cpu_so1_1s2024"]="cpu"
-)
+cd ..
 
-# Ejecutar Makefile para compilar los módulos
-for module_name in "${!module_info[@]}"; do
-    make -C "$script_dir/${module_info[$module_name]}" all
-    if [ $? -ne 0 ]; then
-        echo "Error de compilación para el módulo $module_name en ${module_info[$module_name]}. Abortando carga de módulos y eliminación de archivos."
-        exit 1
-    fi
-done
-
-echo "Compilación exitosa. Cargando los módulos..."
-
-# Cargar los módulos
-for module_name in "${!module_info[@]}"; do
-    sudo insmod "$script_dir/${module_info[$module_name]}/${module_name}.ko"
-    if [ $? -ne 0 ]; then
-        echo "Error al cargar el módulo $module_name. Abortando eliminación de archivos."
-        exit 1
-    fi
-done
-
-echo "Módulos cargados correctamente. Eliminando archivos..."
-
-# Eliminar archivos generados
-for module_name in "${!module_info[@]}"; do
-    make -C "$script_dir/${module_info[$module_name]}" clean
-done
-
-echo "Proceso completado."
+cd cpu/
+make all
+sudo insmod cpu_module.ko
+make clean
