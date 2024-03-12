38810465965fd99051decaca8aba09836785cf1f
danielcuque <3024465830102@ingenieria.usac.edu.gt>
2024-03-11 22:57:18 -0600

    feat: line chart

--- Proyectos/Proyecto1/front/app/(app)/historico/page.tsx
+++ Proyectos/Proyecto1/front/app/(app)/historico/page.tsx
@@ -1,7 +1,48 @@
+"use client";
+
+import { LineChart } from "@/components/Graph/Graph";
+
 export default function Historico() {
+
+    const nowStr = new Date().toLocaleString();
+
     return (
         <div>
             <h1 className="text-3xl font-semibold text-center">Registros históricos</h1>
+            <div className="w-full flex flex-col h-full gap-16 justify-around mt-4">
+                <div>
+                    <h2 className="text-xl text-center mt-4">Memoria RAM</h2>
+                    {
+                        <LineChart data={[0, 100, 200, 120, 20, 10, 23]} labels={[
+                            nowStr,
+                            nowStr,
+                            nowStr,
+                            nowStr,
+                            nowStr,
+                            nowStr,
+                        ]} 
+                        
+                        dataSetLabel="Uso de RAM"
+                        />
+                    }
+                </div>
+                <div>
+                    <h2 className="text-xl text-center mt-4">CPU</h2>
+                    {
+                        <LineChart data={[0, 100, 200, 120, 20, 10, 23]} labels={[
+                            nowStr,
+                            nowStr,
+                            nowStr,
+                            nowStr,
+                            nowStr,
+                            nowStr,
+                        ]} 
+                        
+                        dataSetLabel="Uso de CPU"
+                        />
+                    }
+                </div>
+            </div>
         </div>
     );
 }
--- Proyectos/Proyecto1/front/components/Graph/Graph.tsx
+++ Proyectos/Proyecto1/front/components/Graph/Graph.tsx
@@ -45,3 +45,49 @@ export const MemoryChart: React.FC<MemoryChartProps> = ({ data, labels }) => {
         </div>
     );
 };
+
+interface LineChartProps extends MemoryChartProps {
+    dataSetLabel: string
+}
+
+
+export const LineChart: React.FC<LineChartProps> = ({ data, labels, dataSetLabel }) => {
+    const chartRef = useRef<HTMLCanvasElement | null>(null);
+
+    useEffect(() => {
+
+        if (!chartRef.current) return;
+
+        const ctx = chartRef.current.getContext('2d');
+
+        if (!ctx) return;
+
+        const myChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels,
+                datasets: [{
+                    label: dataSetLabel,
+                    data,
+                    fill: false,
+                    borderColor: 'rgb(75, 192, 192)',
+                    tension: 0.1
+                }]
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+            }
+        });
+        return () => {
+            myChart.destroy();
+        };
+
+    }, [data]);
+
+    return (
+        <div>
+            <canvas ref={chartRef}></canvas>
+        </div>
+    );
+}
