3c75356a8413981175fc8b7b071ec5248306edae
danielcuque <3024465830102@ingenieria.usac.edu.gt>
2024-03-16 16:06:54 -0600

    parse response

--- Proyectos/Proyecto1/front/utils/api.ts
+++ Proyectos/Proyecto1/front/utils/api.ts
@@ -1,23 +1,21 @@
 export const getInfo = async <T= any>(url: string): Promise<T> => {
     try {
-        const response = await fetch(
-            url,
-            {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json'
-                }
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
             }
-        )
+        });
+
         if (response.ok) {
             const data = await response.json();
             console.log(data);
             return data;
+        } else {
+            throw new Error('La solicitud no fue exitosa');
         }
-
     } catch (error) {
-        console.log(error);
-    } finally {
-        return {} as T;
+        console.error('Error en la solicitud:', error);
+        throw error;
     }
 }
