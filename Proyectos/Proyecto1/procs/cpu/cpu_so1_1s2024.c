5ea75f40e49b1304e59a3885b1478f7f1418f6f4
danielcuque <3024465830102@ingenieria.usac.edu.gt>
2024-03-16 17:51:18 -0600

    cpu module

--- Proyectos/Proyecto1/procs/cpu/cpu_so1_1s2024.c
+++ Proyectos/Proyecto1/procs/cpu/cpu_so1_1s2024.c
@@ -1,4 +1,3 @@
-// Info de los módulos
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/sched.h>
@@ -13,7 +12,7 @@
 #include <linux/timekeeping.h>
 #include <linux/time.h>
 
-struct task_struct *cpu; // Estructura que almacena info del cpu
+struct task_struct *cpu;
 unsigned long rss;
 
 
@@ -30,8 +29,8 @@ static int mostrar_informacion_cpu(struct seq_file *archivo, void *v) {
     uint64_t total_usage_ns;
     unsigned long cpu_porcentaje;
 
-    total_cpu_time_ns = 0; // Inicializa a cero
-    total_usage_ns = 0;    // Inicializa a cero
+    total_cpu_time_ns = 0; 
+    total_usage_ns = 0;    
     cpu_porcentaje=0;
 
     for_each_process(cpu) {
@@ -50,8 +49,8 @@ static int mostrar_informacion_cpu(struct seq_file *archivo, void *v) {
 
     seq_printf(archivo, "{\n");
     
-    seq_printf(archivo, "  \"porcentaje_utilizacion_cpu\": %ld,\n", cpu_porcentaje);
-    seq_printf(archivo, "  \"procesos\": [\n");
+    seq_printf(archivo, "  \"cpuUsed\": %ld,\n", cpu_porcentaje);
+    seq_printf(archivo, "  \"processes\": [\n");
 
     int first_process = 1;
 
@@ -62,10 +61,10 @@ static int mostrar_informacion_cpu(struct seq_file *archivo, void *v) {
         first_process = 0;
 
         seq_printf(archivo, "    {\n");
-        seq_printf(archivo, "      \"ID_PROCESO\": %d,\n", cpu->pid);
+        seq_printf(archivo, "      \"processId\": %d,\n", cpu->pid);
         seq_printf(archivo, "      \"PID\": %d,\n", cpu->pid);
-        seq_printf(archivo, "      \"Nombre\": \"%s\",\n", cpu->comm);
-        seq_printf(archivo, "      \"Estado\": %u,\n", cpu->__state);
+        seq_printf(archivo, "      \"Name\": \"%s\",\n", cpu->comm);
+        seq_printf(archivo, "      \"State\": %u,\n", cpu->__state);
 
         if (cpu->mm) {
             rss = get_mm_rss(cpu->mm) << PAGE_SHIFT;
@@ -78,11 +77,11 @@ static int mostrar_informacion_cpu(struct seq_file *archivo, void *v) {
 
         // Verificar si el proceso tiene hijos
         if (!list_empty(&cpu->children)) {
-            seq_printf(archivo, "      \"hijos\": [\n");
+            seq_printf(archivo, "      \"children\": [\n");
             imprimir_hijos(archivo, &cpu->children);
             seq_printf(archivo, "\n      ]");
         } else {
-            seq_printf(archivo, "      \"hijos\": []");
+            seq_printf(archivo, "      \"children\": []");
         }
 
         seq_printf(archivo, "\n    }");
@@ -108,19 +107,19 @@ static void imprimir_hijos(struct seq_file *archivo, struct list_head *hijos) {
         child = list_entry(lstProcess, struct task_struct, sibling);
 
         seq_printf(archivo, "        {\n");
-        seq_printf(archivo, "          \"ID_PROCESO_HIJO\": %d,\n", child->pid);
-        seq_printf(archivo, "          \"PID_HIJO\": %d,\n", child->pid);
-        seq_printf(archivo, "          \"Nombre_HIJO\": \"%s\",\n", child->comm);
-        seq_printf(archivo, "          \"Estado_HIJO\": %u,\n", child->__state);
+        seq_printf(archivo, "          \"childrenProcessId\": %d,\n", child->pid);
+        seq_printf(archivo, "          \"childrenPID\": %d,\n", child->pid);
+        seq_printf(archivo, "          \"childrenName\": \"%s\",\n", child->comm);
+        seq_printf(archivo, "          \"childrenState\": %u,\n", child->__state);
 
         if (child->mm) {
             rss = get_mm_rss(child->mm) << PAGE_SHIFT;
-            seq_printf(archivo, "          \"RSS_HIJO\": %lu,\n", rss);
+            seq_printf(archivo, "          \"childrenRSS\": %lu,\n", rss);
         } else {
-            seq_printf(archivo, "          \"RSS_HIJO\": null,\n");
+            seq_printf(archivo, "          \"childrenRSS\": null,\n");
         }
 
-        seq_printf(archivo, "          \"UID_HIJO\": %u\n", from_kuid(&init_user_ns, child->cred->user->uid));
+        seq_printf(archivo, "          \"childrenUID\": %u\n", from_kuid(&init_user_ns, child->cred->user->uid));
         seq_printf(archivo, "        }");
     }
 }
