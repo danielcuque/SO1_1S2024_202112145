3f3c314c4f02343de96df026ec7b400f4fcd9900
danielcuque <3024465830102@ingenieria.usac.edu.gt>
2024-03-16 20:00:06 -0600

    fix: structure for database

--- Proyectos/Proyecto1/procs/cpu/cpu_so1_1s2024.c
+++ Proyectos/Proyecto1/procs/cpu/cpu_so1_1s2024.c
@@ -31,23 +31,23 @@ static void agregar_hijos(struct task_struct *parent, struct process_info *node)
     struct process_info *child_node;
 
     list_for_each_entry(child, &parent->children, sibling) {
-        child_node = kmalloc(sizeof(struct process_info), GFP_KERNEL); // Allocate memory for child_node
+        child_node = kmalloc(sizeof(struct process_info), GFP_KERNEL);
         if (!child_node) {
             printk(KERN_ERR "Error al asignar memoria para child_node\n");
             return;
         }
-        INIT_LIST_HEAD(&child_node->children); // Initialize the children list for child_node
+        INIT_LIST_HEAD(&child_node->children);
         child_node->pid = child->pid;
         strncpy(child_node->name, child->comm, TASK_COMM_LEN - 1);
         child_node->rss = (child->mm) ? (get_mm_rss(child->mm) << PAGE_SHIFT) : 0;
         child_node->uid = from_kuid(&init_user_ns, child->cred->user->uid);
-        list_add_tail(&child_node->children, &node->children); // Add child_node to the children list of node
-        agregar_hijos(child, child_node); // Recursively add children of child
+        list_add_tail(&child_node->children, &node->children);
+        agregar_hijos(child, child_node);
     }
 }
 
 static void inicializar_arbol(void) {
-    INIT_LIST_HEAD(&root.children); // Initialize the children list of root
+    INIT_LIST_HEAD(&root.children);
     root.pid = 0;
     strncpy(root.name, "root", TASK_COMM_LEN - 1);
     root.rss = 0;
@@ -56,68 +56,67 @@ static void inicializar_arbol(void) {
 }
 
 static void imprimir_procesos(struct seq_file *archivo, struct process_info *node, int nivel) {
-    // Print process information
-    int i;
-    for (i = 0; i < nivel; i++) {
-        seq_printf(archivo, "    ");
-    }
-
-    seq_printf(archivo, "{\n");
-    for (i = 0; i < nivel + 1; i++) {
-        seq_printf(archivo, "    ");
-    }
-    seq_printf(archivo, "\"processId\": %d,\n", node->pid);
-    for (i = 0; i < nivel + 1; i++) {
-        seq_printf(archivo, "    ");
-    }
-    seq_printf(archivo, "\"PID\": %d,\n", node->pid);
-    for (i = 0; i < nivel + 1; i++) {
-        seq_printf(archivo, "    ");
-    }
-    seq_printf(archivo, "\"Name\": \"%s\",\n", node->name);
-    for (i = 0; i < nivel + 1; i++) {
-        seq_printf(archivo, "    ");
-    }
-    seq_printf(archivo, "\"RSS\": %lu,\n", node->rss);
-    for (i = 0; i < nivel + 1; i++) {
-        seq_printf(archivo, "    ");
-    }
-    seq_printf(archivo, "\"UID\": %d,\n", node->uid);
-    for (i = 0; i < nivel + 1; i++) {
-        seq_printf(archivo, "    ");
-    }
-
-    if (!list_empty(&node->children)) {
-        seq_printf(archivo, "\"children\": [\n");
-        struct process_info *child_node;
-        list_for_each_entry(child_node, &node->children, children) {
-            imprimir_procesos(archivo, child_node, nivel + 1);
-            if (list_is_last(&child_node->children, &node->children)) {
-                seq_printf(archivo, "\n");
-            } else {
-                seq_printf(archivo, ",\n");
-            }
+    struct process_info *child_node;
+    list_for_each_entry(child_node, &node->children, children) {
+        int i;
+        for (i = 0; i < nivel; i++) {
+            seq_printf(archivo, "    ");
         }
+
+        seq_printf(archivo, "{\n");
         for (i = 0; i < nivel + 1; i++) {
             seq_printf(archivo, "    ");
         }
-        seq_printf(archivo, "]");
-    } else {
-        seq_printf(archivo, "\"children\": []");
-    }
+        seq_printf(archivo, "\"processId\": %d,\n", child_node->pid);
+        for (i = 0; i < nivel + 1; i++) {
+            seq_printf(archivo, "    ");
+        }
+        seq_printf(archivo, "\"PID\": %d,\n", child_node->pid);
+        for (i = 0; i < nivel + 1; i++) {
+            seq_printf(archivo, "    ");
+        }
+        seq_printf(archivo, "\"Name\": \"%s\",\n", child_node->name);
+        for (i = 0; i < nivel + 1; i++) {
+            seq_printf(archivo, "    ");
+        }
+        seq_printf(archivo, "\"RSS\": %lu,\n", child_node->rss);
+        for (i = 0; i < nivel + 1; i++) {
+            seq_printf(archivo, "    ");
+        }
+        seq_printf(archivo, "\"UID\": %d,\n", child_node->uid);
+        for (i = 0; i < nivel + 1; i++) {
+            seq_printf(archivo, "    ");
+        }
 
-    seq_printf(archivo, "\n");
-    for (i = 0; i < nivel; i++) {
-        seq_printf(archivo, "    ");
-    }
-    seq_printf(archivo, "}");
+        if (!list_empty(&child_node->children)) {
+            seq_printf(archivo, "\"children\": [\n");
+            imprimir_procesos(archivo, child_node, nivel + 1);
+            seq_printf(archivo, "\n");
+            for (i = 0; i < nivel + 1; i++) {
+                seq_printf(archivo, "    ");
+            }
+            seq_printf(archivo, "]");
+        } else {
+            seq_printf(archivo, "\"children\": []");
+        }
 
-    return;
+        seq_printf(archivo, "\n");
+        for (i = 0; i < nivel; i++) {
+            seq_printf(archivo, "    ");
+        }
+        seq_printf(archivo, "}");
+
+        if (list_is_last(&child_node->children, &node->children)) {
+            seq_printf(archivo, "\n");
+        } else {
+            seq_printf(archivo, ",\n");
+        }
+    }
 }
 
 static int mostrar_informacion_cpu(struct seq_file *archivo, void *v) {
-    inicializar_arbol(); // Initialize the tree structure
-    imprimir_procesos(archivo, &root, 0); // Print process information recursively
+    inicializar_arbol();
+    imprimir_procesos(archivo, &root, 0);
     return 0;
 }
 
@@ -126,12 +125,11 @@ static int abrir_archivo(struct inode *inode, struct file *file)
     return single_open(file, mostrar_informacion_cpu, NULL);
 }
 
-static const struct file_operations operaciones = {
-    .owner = THIS_MODULE,
-    .open = abrir_archivo,
-    .read = seq_read,
-    .llseek = seq_lseek,
-    .release = single_release,
+static const struct proc_ops operaciones = {
+    .proc_open = abrir_archivo,
+    .proc_read = seq_read,
+    .proc_lseek = seq_lseek,
+    .proc_release = single_release,
 };
 
 static int __init cargar_modulo(void)
