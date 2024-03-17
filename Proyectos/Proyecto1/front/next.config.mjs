b613589cd0be46442ae0d15cc9dddac2711d41e4
danielcuque <3024465830102@ingenieria.usac.edu.gt>
2024-03-17 17:54:09 -0600

    graph simulation

--- Proyectos/Proyecto1/front/next.config.mjs
+++ Proyectos/Proyecto1/front/next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
+    async rewrites() {
+        const proxy = process.env.PROXY_IP || 'http://192.168.1.23';
+        return [
+            {
+                source: '/api/:path*',
+                destination: `${proxy}:8080/api/:path*`,
+            },
+        ];
+    }
 };
 
 export default nextConfig;
