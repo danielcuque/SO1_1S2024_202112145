eba33cd82c8470a0276424fee6cd7cf9b444a830
danielcuque <3024465830102@ingenieria.usac.edu.gt>
2024-04-23 18:17:22 -0600

    grcp

---
+++ Proyectos/Proyecto2/grpc/server/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	pb "grpc-server/proto"
+	"log"
+	"net"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
+	"google.golang.org/grpc"
+)
+
+var ctx = context.Background()
+var db *sql.DB
+
+type server struct {
+	pb.UnimplementedGetInfoServer
+}
+
+const (
+	port = ":3001"
+)
+
+type Data struct {
+	Name  string
+	Album string
+	Year  string
+	Rank  string
+}
+
+func mysqlConnect() {
+	errEnv := godotenv.Load(".env")
+	if errEnv != nil {
+		log.Fatalln("Error al cargar el archivo .env")
+	}
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPassword, dbHost, dbPort, dbName)
+
+	var err error
+	db, err = sql.Open("mysql", dsn)
+	if err != nil {
+		fmt.Println("Error al conectar a MySQL")
+		log.Fatalln(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		fmt.Println("Error al hacer ping a MySQL")
+		log.Fatalln(err)
+	}
+	fmt.Println("Conexión a MySQL exitosa")
+}
+
+func (s *server) ReturnInfo(ctx context.Context, in *pb.RequestId) (*pb.ReplyInfo, error) {
+	fmt.Println("Recibí de cliente: ", in.GetName())
+	data := Data{
+		Name:  in.GetName(),
+		Album: in.GetAlbum(),
+		Year:  in.GetYear(),
+		Rank:  in.GetRank(),
+	}
+	fmt.Println(data)
+	insertMySQL(data)
+	return &pb.ReplyInfo{Info: "Hola cliente, recibí el comentario"}, nil
+}
+
+func insertMySQL(voto Data) {
+	query := "INSERT INTO votos (name, album, year, `rank`) VALUES (?, ?, ?, ?)"
+	_, err := db.ExecContext(ctx, query, voto.Name, voto.Album, voto.Year, voto.Rank)
+	if err != nil {
+		log.Println("Error al insertar en MySQL:", err)
+	}
+}
+
+func main() {
+	mysqlConnect()
+
+	listen, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGetInfoServer(s, &server{})
+
+	if err := s.Serve(listen); err != nil {
+		log.Fatalln(err)
+	}
+
+}
